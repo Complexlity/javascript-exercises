7bf1a80693a1b9fbef0b6dd8320b321469f89550
Complexlity <nwalozielijah@gmail.com>
2022-09-06 23:47:49 +0100

    Solve Palindromes Exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,11 @@
-const palindromes = function () {
-
+const palindromes = function (myString) {
+   let stringMain =  myString.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()\s]/g,"")
+   let stringReversed = stringMain.split('').reverse().join('')
+//    console.log(stringMain, '\n', stringReversed)
+   return stringMain == stringReversed
+   
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
