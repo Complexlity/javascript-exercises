4ca22bfaebaa3bb834c53b70073a87b054ab2428
Complexlity <nwalozielijah@gmail.com>
2022-09-07 18:31:45 +0100

    Solve fibonacci using recursion

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,10 @@
-const fibonacci = function() {
-
+function fibonacci(n) {
+    if (n < 1) return 'OOPS'
+    if ((n == 1) || (n == 2))return 1
+    else return  fibonacci(n-1) + fibonacci(n-2)
 };
+
+
 
 // Do not edit below this line
 module.exports = fibonacci;
