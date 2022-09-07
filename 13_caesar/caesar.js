b54d9912941eb1c7b4e07010fcf4926a9afa7fae
Complexlity <nwalozielijah@gmail.com>
2022-09-07 23:46:17 +0100

    Solve ceaser cypher exercise

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,6 +1,22 @@
-const caesar = function() {
+const caesar = function(myString, key) {
 
+    key = (key + 26) % 26
+    let newString = ''
+    for (let char of  myString) {
+       newString += shiftby(char, key) 
+    }
+  return newString
 };
+
+function shiftby(character, key){
+    if (character.match(/[^a-z]/i)) return character
+    asciiNumber = character.charCodeAt(0)
+    let cap = 90;
+    if (character.toLowerCase() === character) cap = 122
+    newAsciiNumber = asciiNumber + key;
+    if ( newAsciiNumber > cap) newAsciiNumber -= 26
+    return  String.fromCharCode(newAsciiNumber)
+}
 
 // Do not edit below this line
 module.exports = caesar;
