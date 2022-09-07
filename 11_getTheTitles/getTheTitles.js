0ec1dce29a5b016b1a577108931ff04194592174
Complexlity <nwalozielijah@gmail.com>
2022-09-07 18:40:51 +0100

    Solve getTheTitle exercise

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,7 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+    titleArr = []
+    for (let i of arr) titleArr.push(i['title'])  
+    return titleArr
 };
 
 // Do not edit below this line
