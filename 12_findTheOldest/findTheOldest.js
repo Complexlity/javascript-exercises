cec5cf85e720f93f1b5f9771fe1eb696ad7d10e8
Complexlity <nwalozielijah@gmail.com>
2022-09-07 19:59:57 +0100

    Solve findTheOldest exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let highestAge = 0
+    let oldest_person;
+    let thisYear = new Date().getFullYear()
+    for (let i of arr){
+       const yearOfBirth = i.yearOfBirth
+       const yearOfDeath = i.yearOfDeath || thisYear
+       const age = yearOfDeath - yearOfBirth
+        // console.log(`${i.name} - ${age}`)
+       if (age > highestAge) {
+        highestAge = age
+        oldest_person = i
+    }
+    
+}
+return oldest_person
 };
 
 // Do not edit below this line
